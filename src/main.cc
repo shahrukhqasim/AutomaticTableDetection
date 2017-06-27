9c18a79dd5b0e0f3bf104537f532511a57382ed1
Shah Rukh Qasim <ishahrukhqasim@gmail.com>
2017-06-27 20:06:34 +0200

    feat: Read image

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+#include "opencv2/core.hpp"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+using namespace cv;
+
+int main(int argc, char**argv) {
+    if(argc!=2) {
+        cout<<"Improper arguments \n";
+        return -1;
+    }
+
+    string imageFileName = argv[1];
+    Mat image;
+    image = imread(imageFileName, 0);
+
+
     return 0;
 }
